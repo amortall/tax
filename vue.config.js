1ac5058b463f4d1fca13d15647e47a543fa5d973
amortall <18956945585@163.com>
2023-11-08 13:54:48 +0800

    更改

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+    devServer: {
+        proxy: {
+            '^/api': {
+                target: 'http://localhost:9090/',//接口的前缀
+                ws:true,//代理websocked
+                changeOrigin:true,//虚拟的站点需要更管origin
+                pathRewrite:{
+                    '^/api':''//重写路径
+                }
+            }
+        }
+    },
+    transpileDependencies: true
 })
